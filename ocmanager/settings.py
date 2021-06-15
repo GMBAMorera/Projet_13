67f227b71007da2cc07493ef60a3c296f76f8a33
GMBAMorera <morera.georges@hotmail.fr>
2021-06-15 06:42:58 +0200

    minor bugs fixed.

--- ocmanager/settings.py
+++ ocmanager/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-($ywfu_$d2hx9y=hii(2a-qyi96bodv#owem1q-n5a%prjpq+w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/static/'
 STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
